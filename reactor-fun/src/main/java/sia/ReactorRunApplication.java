0103c61a2bf1d195b5336cc2ec6a384d237531e0
tanya <tanya-pvt@mail.ru>
2024-03-24 19:52:47 +0300

    chapter 10 reactor simple

---
+++ reactor-fun/src/main/java/sia/ReactorRunApplication.java
@@ -0,0 +1,13 @@
+package sia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactorRunApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactorRunApplication.class, args);
+    }
+
+}
