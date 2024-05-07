bf9fac3d46135bbe74ca745735561fedd6a97098
tanya <tanya-pvt@mail.ru>
2024-05-07 14:18:15 +0300

    chapter 11-13 spring cloud and reactive mongo feign

--- ingredient-client/src/main/java/tacos/ingredientclient/feign/IngredientClient.java
+++ ingredient-client/src/main/java/tacos/ingredientclient/feign/IngredientClient.java
@@ -4,15 +4,17 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 import tacos.ingredientclient.Ingredient;
 
 @FeignClient("ingredient-service")
 public interface IngredientClient {
 
   @GetMapping("/ingredients/{id}")
-  Ingredient getIngredient(@PathVariable("id") String id);
+  Mono<Ingredient> getIngredient(@PathVariable("id") String id);
 
   @GetMapping("/ingredients")
-  Iterable<Ingredient> getAllIngredients();
+  Flux<Ingredient> getAllIngredients();
 
 }
