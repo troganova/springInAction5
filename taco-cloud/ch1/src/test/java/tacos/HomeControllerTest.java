2d7c0db208a4780247e4ebe859174a95cb7f06fc
tanya <tanya-pvt@mail.ru>
2025-06-21 19:23:11 +0300

    chapter 1

--- taco-cloud/ch1/src/test/java/tacos/HomeControllerTest.java
+++ taco-cloud/ch1/src/test/java/tacos/HomeControllerTest.java
@@ -11,6 +11,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @WebMvcTest(HomeController.class)
 public class HomeControllerTest {
+
     @Autowired
     private MockMvc mockMvc;
 
