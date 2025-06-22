48c228b35f48bddcc9bff5b4d8aab98dcfaec716
tanya <tanya-pvt@mail.ru>
2025-06-22 18:07:50 +0300

    chapter 2

--- taco-cloud/ch2/src/test/java/tacos/Ch2ApplicationTests.java
+++ taco-cloud/ch2/src/test/java/tacos/Ch2ApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class TacoCloudApplicationTests {
+class Ch2ApplicationTests {
 
     @Test
     void contextLoads() {
